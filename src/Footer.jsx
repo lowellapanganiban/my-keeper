12f881917efd77e308f283a29a6ddc181f88ddb2
Lowella <77447379+lowellapanganiban@users.noreply.github.com>
2023-03-01 18:18:56 +0800

    keeper-app-part-1

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+
+
+const Footer = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    return (
+        <footer>
+             <p>Copyright {year}</p>
+        </footer>
+
+    )
+}
+
+
+
+
+
+export default Footer;
---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+
+
+const Note = () => {
+    return (
+    <div className="Note">
+        <h1> Title</h1>
+        <p> Content</p>
+    </div>
+    )
+}
+
+export default Note;
